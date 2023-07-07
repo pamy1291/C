71ba5cbd17accf4080afd99dfb0610d59f5eb43d
Pamy <97935008+pamy1291@users.noreply.github.com>
2023-07-07 19:03:21 +0800

    Update rand.c
    随机数（1-n）

--- rand.c
+++ rand.c
@@ -1,1 +1,18 @@
+#include <stdio.h>                                                          
+#include <stdlib.h>
+#include <time.h>
 
+/*
+ *n：随机 1-n 的数
+ */
+void rand_n(size_t n) {
+    srand((unsigned int)time(NULL));//随机数种子
+    for(int i=0; i<n; i++) {
+        printf("%d ",1+rand()%(n));
+    }
+}
+
+int main(int argc,const char* argv[]) {
+    rand_n(5);//测试
+    return 0;
+}
